e6c09010f9e92504e04a7f0d88aa2a270d11b442
acanyanwu <51096122+acanyanwu@users.noreply.github.com>
2019-06-18 11:02:18 -0500

    Update SecondLargest.java
    mistakenly called it second largest instead of second smallest

--- src/labs/prob2/SecondLargest.java
+++ src/labs/prob2/SecondLargest.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class SecondLargest {
+public class SecondLSmallest{
 	//Fix this code
 	public static List<Integer> getTestList() {
 	    return Stream.generate(Math::random)
@@ -15,11 +15,11 @@ public class SecondLargest {
 	}
 	
 	/**
-	 * Retrieves the second largest element using natural order search
+	 * Retrieves the second smallest element using natural order search
 	 * @param list
 	 * @return
 	 */
-	public static <T extends Comparable<T>> Optional<? extends T> getSecondLargest(List<? extends T> list) { 
+	public static <T extends Comparable<T>> Optional<? extends T> getSecondSmallest(List<? extends T> list) { 
 	    return list.stream()
 	        .sorted()      // since T extends comparable, sorted is sufficient for sorting
 	        .skip(1)          // skip first element to get to the send largest.
@@ -30,13 +30,13 @@ public class SecondLargest {
 	public static void main(String[] args) {
 		List<Integer> intValues = getTestList();
 		System.out.println("Using list: " + intValues);
-		System.out.println("second largest integer is: " + getSecondLargest(intValues).get());
+		System.out.println("second largest integer is: " + getSecondSmallest(intValues).get());
 		System.out.println();
 		
 		List<LocalDate> dates = Arrays.asList(LocalDate.now(), LocalDate.now().minusDays(10), 
 		    LocalDate.now().minusDays(5), LocalDate.now().minusDays(7));
         System.out.println("Using date list: " + dates);
-        System.out.println("second largest date is: " + getSecondLargest(dates).get());
+        System.out.println("second largest date is: " + getSecondSmallest(dates).get());
         System.out.println();
         
 	}
